160cfd8dcb06209ef5170c2c27fb35ef1f87114f
mdgeus <mdgeus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-27 14:12:09 +0000

    tagged version 1.1.5
    git-svn-id: https://plugins.svn.wordpress.org/dbtools/trunk@1207535 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tools/backup.php
@@ -0,0 +1,135 @@
+<?php
+# Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+$dir = plugin_dir_path( __FILE__ );
+$backupdir = str_replace('\tools', '\backups' ,$dir);
+$backupdir = str_replace('/tools', '/backups' ,$backupdir);
+
+echo '<div class="wrap">';
+echo '<table width="400px" border="0">';
+echo '<tr>';
+echo '<td colspan="2">Available backups:</td>';
+echo '</tr>';
+if ($dh = opendir($backupdir)) {
+    while (false !== ($entry = readdir($dh))) {
+        if ($entry != "." && $entry != ".." && $entry != "index.html") {
+        echo '<tr><form method="post" action="">';
+        echo '<td>'.$entry,'</td>';
+        echo '<td><input type="hidden" name="file" id="file" value="'.$entry.'" />';
+        echo '<input type="submit" name="del" id="del" value="del" /></td>';
+        //echo '<td><input type="submit" name="open" id="open" value="open" /></td>';
+        echo '</form></tr>';
+        }
+    }
+closedir($dh);
+}
+echo '</table>';
+echo '<hr />';
+
+?>
+</form>
+
+
+<form method="post" action="">
+    <label for="go">Backup Database <?php echo DB_NAME; ?> ?</label>
+    <button type="submit" name="go" id="go" value="go">Yes</button>
+</form>
+<?php
+if (isset($_POST['del'])) {
+    if (!unlink($backupdir.$_POST['file'])) {
+        echo '<b>Could not delete</b> file '.$_POST['file'].'<br />';
+    }
+    else {
+        #
+        echo 'Backup file removed.<br />';
+		echo '<script type="text/javascript">
+		 window.location.href = "'.admin_url( "admin.php?page=dbtools_backup").'";
+		</script>';
+    }
+}
+if (isset($_POST['go'])) {
+$tofile = "";
+$query = "SHOW TABLES FROM ".DB_NAME." LIKE '" . $wpdb->prefix."%'";
+$result = mysql_query($query);
+
+ if (!$result) {
+    echo '<br />DB Error, could not list tables<br />';
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+ }
+ $tofile.="# Backup created with the wordpress plugin DBtools\r\n";
+ $tofile.="# database name: ". DB_NAME . "\r\n\r\n";
+ $tofile.="# tables starting with : ". $wpdb->prefix . "\r\n\r\n";
+  while ($row = mysql_fetch_row($result)) {
+    $table = $row[0];
+    $s = mysql_query('SELECT * FROM '.$table);
+    $num_fields = mysql_num_fields($s);
+    $tofile .= 'DROP TABLE IF EXISTS '.$table.';'."\r\n";
+	//$tofile .= 'CREATE TABLE IF NOT EXISTS '.$table.';'."\r\n\r\n";
+    $cr = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+    $tofile.= "\r\n\r\n".$cr[1].";\r\n\r\n";
+    for ($i = 0; $i < $num_fields; $i++)  {
+	    while($row = mysql_fetch_row($s)) {
+				$tofile.= 'INSERT INTO '.$table.' VALUES(';
+				for($j=0; $j<$num_fields; $j++)  {
+					$row[$j] = addslashes($row[$j]);
+					$row[$j] = str_replace("\r\n\r\n","\r\n",$row[$j]);
+					if (isset($row[$j])) {
+                        $tofile.= '"'.$row[$j].'"' ;
+                    }
+                    else {
+                        $tofile.= '""';
+                    }
+					if ($j<($num_fields-1)) {
+                        $tofile.= ',';
+                    }
+				}
+				$tofile.= ");\r\n";
+			}
+		}
+		$tofile.="\r\n\r\n";
+ }
+
+	# save file
+    $filename = $backupdir.'backup_'.DB_NAME.'_'.date('Y-m-d_His').'.sql';
+
+    if (!$handle = fopen($filename, 'w+')) {
+         echo "Cannot open file ($filename) <br />";
+         exit;
+    }
+     if (fwrite($handle, $tofile) === FALSE) {
+        echo "Cannot write to file ($filename) <br />";
+        exit;
+    }
+    echo "Backup file ($filename) Created.<br />";
+    fclose($handle);
+	echo '<script type="text/javascript">
+	 window.location.href = "'.admin_url( "admin.php?page=dbtools_backup").'";
+	</script>';
+}
+
+if (isset($_POST['open'])) {
+ $file = $_POST['file'];
+ $ffile = $backupdir.$file;
+
+ if (file_exists($ffile)) {
+    ob_clean();
+    flush();
+        header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+        header("Cache-Control: private", false);
+        header("Content-Type: application/octet-stream");
+        header("Content-Disposition: attachment; filename=\"$file\";" );
+        header("Content-Transfer-Encoding: binary");
+    readfile($ffile);
+    exit;
+ }
+}
+//wp-admin/admin.php?page=dbtools_optimize
+?>
+
+</div>
---
+++ tools/optimize.php
@@ -0,0 +1,95 @@
+<?php
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+
+echo '<div class="wrap">';
+?>
+<form method="post" action="">
+    <button type="submit" name="opt" id="opt" value="opt">Optimize ?</button>
+    <label for="opt">Optimize tables (can be slow for InnoDB tables)</label>
+    <br />
+    <button type="submit" name="ana" id="ana" value="ana">Analyze ?</button>
+    <label for="opt">Analyze tables </label>
+    <br />
+    <hr />
+</form>
+<?php
+//echo DB_NAME;
+//$query = "SHOW TABLES FROM " . DB_NAME;
+$query = "SHOW TABLES FROM ".DB_NAME." LIKE '" . $wpdb->prefix."%'";
+//$result = mysql_query($query);
+$result = $wpdb->get_results( $query, ARRAY_N );
+
+//echo $query;
+if (!$result) {
+    echo '<br />DB Error, could not list tables<br />';
+    echo 'Database Error: ' . $wpdb->print_error();
+    exit;
+}
+
+if (isset($_POST['opt'])) {
+
+ echo '<br />';
+	foreach ( $result as $row )  {
+    $opt = "OPTIMIZE TABLE ".$row[0];
+		$go = $wpdb->get_results( $opt, ARRAY_N );
+     if ($go) {
+			 echo 'Table: '. $row[0] .' Optimized<br />';
+		}
+		else {
+			echo 'Table: '. $row[0] .' Skipped<br />';
+		}
+ }
+
+ echo '<br /><br />All tables optimized successfully<br /><hr /><br />';
+}
+
+if (isset($_POST['ana'])) {
+
+    echo '<br />';
+    // while ($row = mysql_fetch_row($result)) {
+		foreach ( $result as $row )  {
+        $opt = "ANALYZE TABLE " . $row[0];
+				$go = $wpdb->get_results( $opt, ARRAY_N );
+				if ($go) {
+        	echo 'Table: ' . $row[0] . ' Analyzed<br />';
+				}
+				else {
+					echo 'Table: ' . $row[0] . ' Skipped<br />';
+				}
+    }
+
+    echo '<br /><br />All tables analyzed successfully<br /><hr /><br />';
+}
+
+#no selection
+#show table data
+?>
+Database name: <b><?php echo DB_NAME; ?></b><br />
+Showing all tables in <?php echo DB_NAME; ?>&nbsp;
+but only the wordpress tables (<?php echo $wpdb->prefix.'####' ?>) will be ussed<br />
+
+<?php
+$stat = "SHOW TABLE STATUS";
+    //$statr = mysql_query($stat);
+		$statr = $wpdb->get_results( $stat, OBJECT );
+?>
+<table width="400px" border="0">
+    <tr>
+<td>Name</td><td>Size</td><td>Rows</td><td>Engine</td>
+</tr>
+<?php
+//while ($rstat = mysql_fetch_array($statr)) {
+foreach ( $statr as $rstat )  {
+	//echo $fivesdraft->post_title;
+    // $size = ($rstat["Data_length"] + $rstat["Index_length"]) / 1024;
+    // echo '<tr><td>' . $rstat['Name'] . '</td><td>' . $size . ' KB.</td><td>' . $rstat['Rows'] . '</td><td>' . $rstat['Engine'] . '</td></tr>';
+		$size = ($rstat->Data_length + $rstat->Index_length) / 1024;
+    echo '<tr><td>' . $rstat->Name . '</td><td>' . $size . ' KB.</td><td>' . $rstat->Rows . '</td><td>' . $rstat->Engine . '</td></tr>';
+}
+?>
+</table>
+</div>
