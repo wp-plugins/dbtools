bc31a0728ec992d3b3486e36ae7bf67adc3291b4
mdgeus <mdgeus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-31 12:34:53 +0000

    version 1.1.6
    git-svn-id: https://plugins.svn.wordpress.org/dbtools/trunk@1210308 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- DBtools.php
+++ DBtools.php
@@ -1,16 +1,16 @@
 <?php
 
 /*
-  Plugin Name: DBtools
-  Plugin URI: https://github.com/mdgeus/WP-DBtools
-  Description: Usefull mysql Database tools
-  Version: 1.1.5
-  Author: Hands Off
-  Author URI: http://dennis.famgeus.nl/
-  License: GPL2
- */
+Plugin Name: DBtools
+Plugin URI: http://dennis.famgeus.nl/dbtools-plugin/
+Description: Createa a backup of your wordpress tables
+Version: 1.1.6
+Author: Hands Off
+Author URI: http://www.hands-off.it/
+License: GPL2
+*/
 
- // Make sure we don't expose any info if called directly
+// Make sure we don't expose any info if called directly
 if ( !function_exists( 'add_action' ) ) {
 	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
 	exit;
@@ -20,14 +20,12 @@ global $wpdb;
 global $current_user;
 
 if (!defined('DBtools_VERSION_KEY')) {
-    define('DBtools_VERSION_KEY', 'DBtools_version');
+	define('DBtools_VERSION_KEY', 'DBtools_version');
 }
 if (!defined('DBtools_VERSION_NUM')) {
-    define('DBtools_VERSION_NUM', '1.1.5');
+	define('DBtools_VERSION_NUM', '1.1.6');
 }
 add_option(DBtools_VERSION_KEY, DBtools_VERSION_NUM);
-
-
 
 $dir = plugin_dir_path( __FILE__ );
 
@@ -35,71 +33,102 @@ $dir = plugin_dir_path( __FILE__ );
 add_action('admin_menu', 'dbtools_menu');
 
 #create the menucode
-function dbtools_menu() {
-  //add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-  add_menu_page('dbtools Options', 'DBtools', 'manage_options', 'dbtools_menu', 'dbtools_options');
-    //add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function);
-    add_submenu_page('dbtools_menu', 'Maintenance', 'Maintenance', 'manage_options', 'dbtools_optimize', 'dbtools_optimize');
-    add_submenu_page('dbtools_menu', 'Backup', 'Backup', 'manage_options', 'dbtools_backup', 'dbtools_backup');
+if ( !function_exists( 'dbtools_menu' ) ) {
+	function dbtools_menu() {
+		//add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+		add_menu_page('dbtools Options', 'DBtools', 'manage_options', 'dbtools_menu', 'dbtools_options');
+		//add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function);
+		add_submenu_page('dbtools_menu', 'Maintenance', 'Maintenance', 'manage_options', 'dbtools_optimize', 'dbtools_optimize');
+		add_submenu_page('dbtools_menu', 'Backup', 'Backup', 'manage_options', 'dbtools_backup', 'dbtools_backup');
+	}
 }
 
+if ( !function_exists( 'dbtools_listdb' ) ) {
+	function dbtools_listdb() {
+		if (!current_user_can('manage_options')) {
+			wp_die(__('You do not have sufficient permissions to access this page.'));
+		}
+		# listing tables
+	}
+}
 
+if ( !function_exists( 'dbtools_del' ) ) {
+	function dbtools_del() {
+		if (!current_user_can('manage_options')) {
+			wp_die(__('You do not have sufficient permissions to access this page.'));
+		}
+		# delete backup file
+		$file = $_GET['fn'];
+		$backupdir = str_replace('\tools', '\backups' ,$dir);
+		$backupdir = str_replace('/tools', '/backups' ,$backupdir);
+		if(unlink($backupdir.$file)) {
+			echo 'Backup file ' . $file . ' removed<br />';
+		}
+		exit;
+	}
+}
 
-function dbtools_listdb() {
-    # listing tables
-    }
-
-
-function dbtools_del() {
-    # delete backup file
-    $file = $_GET['fn'];
-    $backupdir = str_replace('\tools', '\backups' ,$dir);
-    $backupdir = str_replace('/tools', '/backups' ,$backupdir);
-    if(unlink($backupdir.$file)) {
-        echo 'Backup file ' . $file . ' removed<br />';
-    }
-    }
-
-
-function dbtools_optimize() {
-    if (!current_user_can('manage_options')) {
-        wp_die(__('You do not have sufficient permissions to access this page.'));
-    }
-    global $wpdb;
+if ( !function_exists( 'dbtools_optimize' ) ) {
+	function dbtools_optimize() {
+		if (!current_user_can('manage_options')) {
+			wp_die(__('You do not have sufficient permissions to access this page.'));
+		}
+		global $wpdb;
 		global $dir;
-   include($dir.'tools/optimize.php');
-   exit;
+		include($dir.'tools/optimize.php');
+		exit;
+	}
 }
 
-function dbtools_backup() {
-    if (!current_user_can('manage_options')) {
-        wp_die(__('You do not have sufficient permissions to access this page.'));
-    }
-   global $wpdb;
-	global $dir;
-   //$backupdir = $dir.'backups/';
-   include($dir.'tools/backup.php');
-   exit;
+if ( !function_exists( 'dbtools_backup' ) ) {
+	function dbtools_backup() {
+		if (!current_user_can('manage_options')) {
+			wp_die(__('You do not have sufficient permissions to access this page.'));
+		}
+		global $wpdb;
+		global $dir;
+		//$backupdir = $dir.'backups/';
+		include($dir.'tools/backup.php');
+		exit;
+	}
 }
 
+if ( !function_exists( 'dbtools_test' ) ) {
+	function dbtools_test() {
+		echo 'tst';
+	}
+}
+
+if ( !function_exists( 'dbtools_download' ) ) {
+	function dbtools_download() {
+		if (!current_user_can('manage_options')) {
+			wp_die(__('You do not have sufficient permissions to access this page.'));
+		}
+		global $wpdb;
+		global $dir;
+		echo 'Download';
+
+	}
+}
 
 #menu page output here
-function dbtools_options() {
-    if (!current_user_can('manage_options')) {
-        wp_die(__('You do not have sufficient permissions to access this page.'));
-    }
-    echo '<div class="wrap">';
-    echo '<p><b>DB tools</b> are some usefull database tools</p>';
-    echo '<p>You can run the maintanence tools or backup your wordpress database from the menu</p>';
-    echo '<p>Tools included:<br />
-             <ul>
-                <li>Optmimize tables</li>
-                <li>Analyze tables</li>
-                <li>Backup database</li>
-             </ul></p>';
-    echo '<hr />';
-    echo '<p>Version '.DBtools_VERSION_NUM.'</p>';
-    echo '</div>';
+if ( !function_exists( 'dbtools_options' ) ) {
+	function dbtools_options() {
+		if (!current_user_can('manage_options')) {
+			wp_die(__('You do not have sufficient permissions to access this page.'));
+		}
+		echo '<div class="wrap">';
+		echo '<p><b>DB tools</b> are some usefull database tools</p>';
+		echo '<p>You can run the maintanence tools or backup your wordpress database from the menu</p>';
+		echo '<p>Tools included:<br />
+		<ul>
+		<li>Optmimize tables</li>
+		<li>Analyze tables</li>
+		<li>Backup database</li>
+		</ul></p>';
+		echo '<hr />';
+		echo '<p>Version '.DBtools_VERSION_NUM.'</p>';
+		echo '</div>';
+	}
 }
-
 ?>
--- readme.txt
+++ readme.txt
@@ -3,12 +3,13 @@ Contributors: mdgeus
 Tags: tool, tools, database, optimize, backup
 Requires at least: 3.0.1
 Tested up to: 4.2.3
-Stable tag: 1.1.5
+Stable tag: 1.1.6
 License: GPLv2 or later
 
-Wordpress plugin for DB maintenance
+Wordpress plugin for DB maintenance and backup
 
 == Description ==
+Simple tool you can use to backup your Wordpress tables and give an optimize and analyze command.
 
 Functionality:
 * Optimize tables
@@ -30,10 +31,14 @@ Click Activate Plugin to activate it.
 The plugin folder is called DBtools, this is the folder you should upload.
 
 == Screenshots ==
+
 1. Create backups
 2. optimize or analyse the WP tables
 
 == Changelog ==
+
+= 1.1.6 =
+Now you can download your backup file
 
 = 1.1.5 =
 Added - IF NOT EXISTS to the drop table statement
