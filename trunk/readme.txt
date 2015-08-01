d7bc7ae42bfc2cb542cf72d8cafe2e17a5c7948a
mdgeus <mdgeus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-01 06:50:20 +0000

    fixed plugin activation error
    git-svn-id: https://plugins.svn.wordpress.org/dbtools/tags/1.1.9@1210784 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/readme.txt
@@ -0,0 +1,75 @@
+=== DBtools ===
+Contributors: mdgeus
+Tags: tool, tools, database, optimize, backup
+Requires at least: 3.0.1
+Tested up to: 4.2.3
+Stable tag: 1.1.9
+License: GPLv2 or later
+
+Wordpress plugin for DB maintenance and backup
+
+== Description ==
+Simple tool you can use to backup your Wordpress tables and give an optimize and analyze command.
+
+Functionality:
+* Optimize tables
+* Analyze tables
+* database backup
+
+! make sure you test the import of the created backup file, before you rely on the backups !
+
+== Installation ==
+
+To install a WordPress Plugin manually:
+
+Download your WordPress Plugin to your desktop.
+If downloaded as a zip archive, extract the Plugin folder to your desktop.
+Read through the "readme" file thoroughly to insure you follow the installation instructions.
+With your FTP program, upload the Plugin folder to the wp-content/plugins folder in your WordPress directory online.
+Go to Plugins screen and find the newly uploaded Plugin in the list.
+Click Activate Plugin to activate it.
+
+The plugin folder is called DBtools, this is the folder you should upload.
+
+== Screenshots ==
+
+1. Create backups
+2. optimize or analyse the WP tables
+
+== Changelog ==
+
+= 1.1.9 =
+Only perform update when version is lower the 1.1.7
+
+= 1.1.8 =
+Fixed "you have not sufficient permissions error"
+
+= 1.1.7 =
+placed backup location outside the plugin directory so your backups will be kept after deactivating or updating this plugin
+
+= 1.1.6 =
+Now you can download your backup file
+
+= 1.1.5 =
+Added - IF NOT EXISTS to the drop table statement
+
+= 1.1.4 =
+Fixed some bugs and errors
+switched to mysqli
+reload the page after backup
+
+= 1.1.3 =
+Limited analyze and optimize to WP tables only
+
+= 1.1.2 =
+Fixed delete backup file
+Changed backup filename to date_time for multiple backups per day
+
+= 1.1.1 =
+Fixed backup creation
+
+= 1.1.0 =
+Added table list
+Added analyze tables
+
+= 1.0.0
---
+++ trunk/tools/backup.php
@@ -0,0 +1,118 @@
+<?php
+# Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
+}
+$backupdir = ABSPATH.'dbtoolsbackups';
+
+echo '<div class="wrap">';
+echo '<table width="400px" border="0">';
+echo '<tr>';
+echo '<td colspan="2">Available backups:</td>';
+echo '</tr>';
+if ($dh = opendir($backupdir)) {
+	while (false !== ($entry = readdir($dh))) {
+		if ($entry != "." && $entry != ".." && $entry != "index.php") {
+			echo '<tr><form method="post" action="">';
+			echo '<td><a href="'.plugins_url('DBtools/download.php?file='.$entry).'" title="Download SQL file">'.$entry.'</a></td>';
+			echo '<td><input type="hidden" name="file" id="file" value="'.$entry.'" />';
+			echo '<input type="submit" name="del" id="del" value="del" /></td>';
+			echo '</form></tr>';
+		}
+	}
+	closedir($dh);
+}
+echo '</table>';
+echo '<hr />';
+
+?>
+</form>
+
+
+<form method="post" action="">
+	<label for="go">Backup Database <?php echo DB_NAME; ?> ?</label>
+	<button type="submit" name="go" id="go" value="go">Yes</button>
+</form>
+<?php
+if (isset($_POST['del'])) {
+	if (!unlink($backupdir.'/'.$_POST['file'])) {
+		echo '<b>Could not delete</b> file '.$_POST['file'].'<br />';
+	}
+	else {
+		#
+		echo 'Backup file removed.<br />';
+		echo '<script type="text/javascript">
+		window.location.href = "'.admin_url( "admin.php?page=dbtools_backup").'";
+		</script>';
+	}
+}
+
+if (isset($_POST['go'])) {
+	$tofile = "";
+	$query = "SHOW TABLES FROM ".DB_NAME." LIKE '" . $wpdb->prefix."%'";
+	$result = mysql_query($query);
+
+	if (!$result) {
+		echo '<br />DB Error, could not list tables<br />';
+		echo 'MySQL Error: ' . mysql_error();
+		exit;
+	}
+	$tofile.="# Backup created with the wordpress plugin DBtools\r\n";
+	$tofile.="# database name: ". DB_NAME . "\r\n\r\n";
+	$tofile.="# tables starting with : ". $wpdb->prefix . "\r\n\r\n";
+	while ($row = mysql_fetch_row($result)) {
+		$table = $row[0];
+		$s = mysql_query('SELECT * FROM '.$table);
+		$num_fields = mysql_num_fields($s);
+		$tofile .= 'DROP TABLE IF EXISTS '.$table.';'."\r\n";
+		//$tofile .= 'CREATE TABLE IF NOT EXISTS '.$table.';'."\r\n\r\n";
+		$cr = mysql_fetch_row(mysql_query('SHOW CREATE TABLE '.$table));
+		$tofile.= "\r\n\r\n".$cr[1].";\r\n\r\n";
+		for ($i = 0; $i < $num_fields; $i++)  {
+			while($row = mysql_fetch_row($s)) {
+				$tofile.= 'INSERT INTO '.$table.' VALUES(';
+				for($j=0; $j<$num_fields; $j++)  {
+					$row[$j] = addslashes($row[$j]);
+					$row[$j] = str_replace("\r\n\r\n","\r\n",$row[$j]);
+					if (isset($row[$j])) {
+						$tofile.= '"'.$row[$j].'"' ;
+					}
+					else {
+						$tofile.= '""';
+					}
+					if ($j<($num_fields-1)) {
+						$tofile.= ',';
+					}
+				}
+				$tofile.= ");\r\n";
+			}
+		}
+		$tofile.="\r\n\r\n";
+	}
+
+	# save file
+	$filename = $backupdir.'/backup_'.DB_NAME.'_'.date('Y-m-d_His').'.sql';
+
+	if (!$handle = fopen($filename, 'w+')) {
+		echo "Cannot open file ($filename) <br />";
+		exit;
+	}
+	if (fwrite($handle, $tofile) === FALSE) {
+		echo "Cannot write to file ($filename) <br />";
+		exit;
+	}
+	echo "Backup file ($filename) Created.<br />";
+	fclose($handle);
+	echo '<script type="text/javascript">
+	window.location.href = "'.admin_url( "admin.php?page=dbtools_backup").'";
+	</script>';
+}
+
+if (isset($_POST['open'])) {
+	echo '<script type="text/javascript">
+	window.location.href = "'.admin_url("admin.php?page=dbtools_download").'";
+	</script>';
+}
+?>
+</div>
