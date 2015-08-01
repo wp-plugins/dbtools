8692f4a280f55323e28c3523b237999fe657f55e
mdgeus <mdgeus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-01 06:27:07 +0000

    version 1.1.9 changed update if older version
    git-svn-id: https://plugins.svn.wordpress.org/dbtools/tags/1.1.9@1210768 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- download.php
+++ download.php
@@ -4,15 +4,17 @@ if ( !isset( $_GET['file'] ) ) {
     exit;
 }
 if (isset($_GET['file'])) {
+    //$backupdir = ABSPATH.'dbtoolsbackups';
+    $backupdir = '../../../dbtoolsbackups';
     $file_name = $_GET['file'];
     $pattern = '/^backup_[\w]+_[\d]{4}-[\d]{2}-[\d]{2}_[\d]{6}\.sql$/i';
     $checkfile = preg_match($pattern, $file_name);
-
+    //$checkfile = TRUE;
     if ($checkfile) {
         header('Content-Type: text/plain');
         //header("Content-Transfer-Encoding: Binary");
         header("Content-disposition: attachment; filename=\"".$file_name."\"");
-        readfile('./backups/'.$file_name);
+        readfile($backupdir.'/'.$file_name);
         exit;
     }
 }
